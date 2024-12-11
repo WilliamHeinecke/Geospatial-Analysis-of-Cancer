b3201cb7079af939cec87b98b76249ebacdd7def
cole853 <83487099+cole853@users.noreply.github.com>
2024-12-10 23:40:21 -0800

    Update data_analysis.py

--- Python_analysis/data_analysis.py
+++ Python_analysis/data_analysis.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error, mean_absolute_error
 
 # format pandas dataframes
 # pd.set_option('display.max_rows', None)
@@ -16,12 +17,19 @@ class data_analysis:
     def __init__(self, file):
         self.data = pd.read_csv(file)
 
+        self.data.rename(columns={
+            "American Indian/Alaskan Native (includes Hispanic)": "American Indian/Alaskan Native",
+            "Asian/Pacific Islander (includes Hispanic)": "Asian/Pacific Islander",
+            "Black (includes Hispanic)": "Black",
+            "White (includes Hispanic)": "White"
+        }, inplace=True)
+
     # make a correlation matrix for the data file
     def correlation_matrix(self):
         relevant_cols = ["BingeDrinking", "CoronaryHeartDisease",
                      "Diabetes", "Asthma", "Obesity", "BelowPoverty", "Smoking",
-                     "Female", "Male", "American Indian/Alaskan Native (includes Hispanic)",
-                     "Asian/Pacific Islander (includes Hispanic)", "Black (includes Hispanic)", "White (includes Hispanic)"]
+                     "Female", "Male", "American Indian/Alaskan Native",
+                     "Asian/Pacific Islander", "Black", "White"]
 
         if "LiverCancerIncidence" in self.data.columns:
             relevant_cols.append("LiverCancerIncidence")
@@ -35,6 +43,7 @@ class data_analysis:
         plt.figure(figsize=(10, 8))
         sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
         plt.title(f"Correlation Heatmap of Factors and {cancer_type} Cancer Rates")
+        plt.tight_layout()
         plt.show()
 
     # determines which factors are most important for predicting male and female cancer with random forest
@@ -63,10 +72,16 @@ class data_analysis:
         # Evaluate performance
         mse = mean_squared_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
+        mape = mean_absolute_percentage_error(y_test, y_pred)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(y_test, y_pred)
 
         print(f"EVALUATION ON {gender} MODEL")
         print(f"Mean Squared Error: {mse}")
         print(f"R-squared: {r2}")
+        print(f"MAPE: {mape}%")
+        print(f"RMSE: {rmse}")
+        print(f"MAE {mae}")
 
         # get feature importance
         feature_importances = pd.DataFrame({
